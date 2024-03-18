a7ea8877a1edaa73114257af864e4ca4002b31c0
Inti Martínez <58715706+inti2001@users.noreply.github.com>
2024-03-18 16:19:57 -0600

    Add files via upload

---
+++ environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'clevertrip-59cb1',
+    appId: '1:191724323979:web:fd14ad7cf3a9be32e1808e',
+    databaseURL: 'https://clevertrip-59cb1-default-rtdb.firebaseio.com',
+    storageBucket: 'clevertrip-59cb1.appspot.com',
+    apiKey: 'AIzaSyBjxIoEt-kssPuSEktJsbOz0K8jVuOzO-g',
+    authDomain: 'clevertrip-59cb1.firebaseapp.com',
+    messagingSenderId: '191724323979',
+  },
+  production: false
+};
